e4bdfc34ab6ad9464fc1146a045a61cb3852c991
emisan071 <122773047+emisan071@users.noreply.github.com>
2023-02-03 18:22:06 +0900

    質問
    練習問題１

--- 3-2/index.php
+++ 3-2/index.php
@@ -1,42 +1,50 @@
-<?php
+<!-- <?php
 // りんご、みかん、桃のフルーツの単価と個数をもとに料金を計算します
-
-// step1:フルーツと単価の連想配列を作成してください。
-// 配列の0:リンゴ、1:みかん、2:桃の順に個数を配列で作成してください。
-// step2:単価を計算する関数を定義してください。
-// 引数はフルーツの単価・個数の２つ、返り値は計算した合計値を返します。
-// step3:繰り返しを使ってそれぞれのフルーツを書き出してください。
 // りんご300円、みかん150円、桃3000円
 
+// step1:フルーツと単価の連想配列を作成してください。
+// $fruits = ["りんご"=>300,"みかん"=>150,"もも"=>3000
+// ];
+// foreach( $fruits as $key => $value){
 
 
-$fruits = ["りんご"=>300,"みかん"=>150,"もも"=>3000
-];
-foreach ($fruits as $key => $price){ 
-}
-echo $key. "は". $price. "円です。<br>";
-// function gettotal($price, $quantity){
-//     $total = $price * $quantity;
+// // 配列の0:リンゴ、1:みかん、2:桃の順に個数を配列で作成してください。
+// $quantity = [2, 3, 4
+// ];
+
+// // step2:単価を計算する関数を定義してください。
+// // 引数はフルーツの単価・個数の２つ、返り値は計算した合計値を返します。
+// function gettotal($value, $quantity){
+//     $product = $value * $quantity;
+//     return $product;
+//     }
+
+// // step3:繰り返しを使ってそれぞれのフルーツを書き出してください。
+
+//  print  "$key は $product 円です。<br>"; 
 // }
-//     $quantity = 3;
-//     echo $key. "は". $total. "円です。<br>";
+// ?> -->
 
+<?php
+$array = [
+    ["りんご","300"],
+    ["みかん","150"],
+    ["もも","3000"],
+    ["りんご","2"],
+    ["みかん","2"],
+    ["もも","2"]
+];
 
+$keys=array_values(array_unique(array_column($array,0)));
+foreach($keys as $value){
+	$num = 1;
+	foreach($array as $row){
+		if($row[0]==$value){
+			$num = $num * $row[1];
+		}
+	}
+	$total[$value] = $num;
 
-
-    //  function gettotal($value, $quantity){
-    //   $total = $value * $quantity;
-    //   $quantity = 3
-    //   print "$value は $total 円です。";
-    // }
-//     //   return $total;
-//     //   $total = gettotal($value,$quantity);
-    
-//      gettotal(300,1);
-//      print "$key は $total 円です。";
-
-$fruits =["apple"=>"りんご","orange"=>"みかん","peach"=>"もも"];
-foreach ($fruits as $key => $value) {
-    echo $key."といったら".$value.'<br>';
+print  "$value は $num 円です。<br>"; 
 }
 ?>
