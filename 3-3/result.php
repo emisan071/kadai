32e92e52f862a148e7a46c6b71b7c1116341d41d
emisan071 <122773047+emisan071@users.noreply.github.com>
2023-02-11 13:08:31 +0900

    課題提出テスト
    3章練習問題２

--- 3-3/result.php
+++ 3-3/result.php
@@ -1,23 +1,35 @@
 <?php
-$number = $_GET['number'];
-?>
-<?php
+$numbers = $_GET['numbers'];
+$split_str = str_split($numbers);
 
-echo date("Y/m/d", time());
-の運勢は<br>
-<p>選ばれた数字は
-echo mt_rand($number); ?></p><br>
-<?php
-$number = mt_rand($number)
+$number_key = array_rand($split_str,1);
+$value = $split_str[$number_key];
+
+// shuffle($split_str);
+// $number = array_slice($split_str,1,1);
+// var_dump($number);
+
+
+echo date("Y/m/d", time())."の運勢は";
+echo "<br>";
+
+echo "選ばれた数字は". $value;
+
+// foreach($number as $key => $value){
+//     print $value;
+// }
+echo "<br>";
+
+$number = $value;
 if($number === 0){
-    echo '凶';
-} elseif($number = 1,3){
-    echo '小吉';
-} elseif($number = 4,6){
-    echo '中吉';
-} elseif($number = 7,8){
-    echo '吉';
-} elseif($number === 9){
-    echo '大吉';
+    echo "凶";
+} elseif($number <=3){
+    echo "小吉";
+} elseif($number <=6){
+    echo "中吉";
+} elseif($number <=8){
+    echo "吉";
+} else {
+    echo "大吉";
 }
 ?>
